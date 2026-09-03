b9a8e5e31c37c512a70d12fed327b2c16c871039
Shivam Sharma <shivamsharma03052002@gmail.com>
2026-09-04 00:41:36 +0530

    First Program

---
+++ First_Program.py
@@ -0,0 +1,8 @@
+print("Hello World")
+print("My name is Shivam Sharma")
+print("My age is 24")
+
+print("My name is Shivam Sharma","My age is 24")
+print(24)
+print(24+24)
+
---
+++ Variable.py
@@ -0,0 +1,11 @@
+name ="Shivam"
+age= 24
+
+print("My Name is :" ,name)
+print(type(name))
+print(type(age))
+
+a=4
+b=9
+sum =a+b
+print(sum)
